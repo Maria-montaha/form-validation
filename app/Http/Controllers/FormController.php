623313a849cad8efd201757c749f78813baec69a
maria-montaha <mariamontaha2023@gmail.com>
2024-10-28 00:37:31 +0600

    form validation done

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -7,6 +7,14 @@ use Illuminate\Http\Request;
 class FormController extends Controller
 {
     public function addUser(Request $request){
-       return $request->all();
+$request->validate([
+    'username'=>'required|max:55|min:3',
+    'useremail'=>'required|email',
+    'cityname'=>'required',
+    'skill'=>'required',
+]);
+return $request;
+
+    //    return $request->all();
     }
 }
--- resources/views/user-form.blade.php
+++ resources/views/user-form.blade.php
@@ -7,26 +7,39 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
+    {{-- <div> 
+@if ($errors->any())
+@foreach($errors->all() as $error)
+<div style=color:red;>{{$error}}</div>
+@endforeach
+@endif
+
+    </div> --}}
     <form action="addUser" method="POST">
         @csrf
         <div class="container" style="color: rgb(212, 84, 61)";border: 2px solid black>
         <div class="input-form">
             <input type="text" placeholder="enter user name" name="username">
+            <span>@error('username'){{$message}}@enderror</span>
         </div>
         <div class="input-form mt-3">
             <input type="text" placeholder="enter city name" name="cityname">
+            <span>@error('cityname'){{$message}}@enderror</span>
         </div>
         <div class="input-form mt-3">
             <input type="text" placeholder="enter email" name="useremail">
+            <span>@error('useremail'){{$message}}@enderror</span>
         </div>
         <div class="mt-3">
             <h4>User Skills</h4>
             <input type="checkbox" name="skill[]" id="php" value="php">
+            
             <label for="php">PHP</label>
             <input type="checkbox" name="skill[]" id="java" value="java">
             <label for="java">Java</label>
             <input type="checkbox" name="skill[]" id="node" value="node">
             <label for="node">Node</label>
+            <span>@error('skill'){{$message}}@enderror</span>
         </div>
         <div>
             <h4>User Gender</h4>
